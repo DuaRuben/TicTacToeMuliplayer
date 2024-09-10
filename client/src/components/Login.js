2118ba7484d15c3d73e0e4a36243d8c62386af14
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2024-09-10 21:13:20 +0530

    Third Commit
    Changes to css

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
-
+import "./Form.css";
 
 function Login() {
     const [username,setUsername] = useState(null);
@@ -8,15 +8,21 @@ function Login() {
     const login = () => {};
   return (
     <div>
-    <form>
-        <label> label </label>
-        <input placeholder = "Username" onChange = {(event)=> setUsername(event.target.value)}/>
-        <input placeholder = "Password" onChange = {(event)=> setPassword(event.target.value)}/>
-        <button onClick ={login}> Login </button>
+    <form className ="form">
+        <label className = "label"> Login </label>
+
+        <div className ="input-container">
+        <label className = "label"> Username: </label>
+        <input className = "input" placeholder = "Username" onChange = {(event)=> setUsername(event.target.value)}/>
+        </div>
+        <div className ="input-container">
+        <label className = "label"> Password: </label>
+        <input className = "input" placeholder = "Password" onChange = {(event)=> setPassword(event.target.value)}/>
+        </div>
+        <button onClick ={login} className = "button"> Login </button>
     </form>
     <p>Don't have an account? <Link to="/signup">Sign up here</Link></p>
     </div>
-    
   )
 }
 
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-
+import "./Form.css"
 
 function SignUp() {
     const [user,setUser] = useState(null);
     const signUp = () => {};
   return (
     <div>
-        <form>
-            <label> Sign Up </label>
-            <input placeholder = "First Name" onChange = {(event)=> setUser({...user,firstName: event.target.value})}/>
-            <input placeholder = "Last Name" onChange = {(event)=> setUser({...user,lastName: event.target.value})}/>
-            <input placeholder = "Username" onChange = {(event)=> setUser({...user,username: event.target.value})}/>
-            <input placeholder = "Password" onChange = {(event)=> setUser({...user,password: event.target.value})}/>
-            <button onClick ={signUp}> SignUp </button>
+        <form className = "form">
+            <label className = "label"> Sign Up </label>
+            <input className = "input" placeholder = "First Name" onChange = {(event)=> setUser({...user,firstName: event.target.value})}/>
+            <input className = "input" placeholder = "Last Name" onChange = {(event)=> setUser({...user,lastName: event.target.value})}/>
+            <input className = "input" placeholder = "Username" onChange = {(event)=> setUser({...user,username: event.target.value})}/>
+            <input className = "input" placeholder = "Password" onChange = {(event)=> setUser({...user,password: event.target.value})}/>
+            <button className = "button" onClick ={signUp}> SignUp </button>
         </form>
         <p><Link to="/">Back</Link></p>
     </div>
