36c85ab015e6ef78a12c280066710168e1ca3310
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2024-09-10 20:49:12 +0530

    Second Commint
    Changes to SignUp and Login compoents structure and adding Header component with its css

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from "./components/Login"
+import SignUp from "./components/SignUp"
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        < Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+       
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
 
-function login() {
+
+function Login() {
+    const [username,setUsername] = useState(null);
+    const[password,setPassword] = useState(null);
+    const login = () => {};
   return (
-    <div>login</div>
+    <div>
+    <form>
+        <label> label </label>
+        <input placeholder = "Username" onChange = {(event)=> setUsername(event.target.value)}/>
+        <input placeholder = "Password" onChange = {(event)=> setPassword(event.target.value)}/>
+        <button onClick ={login}> Login </button>
+    </form>
+    <p>Don't have an account? <Link to="/signup">Sign up here</Link></p>
+    </div>
+    
   )
 }
 
-export default login
+export default Login
