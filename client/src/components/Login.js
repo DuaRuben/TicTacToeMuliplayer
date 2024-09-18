a290ab06441cae5c745d678a535f61302c315fef
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2024-09-18 10:56:27 +0530

    Login Functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,23 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
+import Axios from "axios";
+import Cookies from "universal-cookie";
 import "./Form.css";
 
 function Login() {
     const [username,setUsername] = useState(null);
-    const[password,setPassword] = useState(null);
-    const login = () => {};
+    const [password,setPassword] = useState(null);
+    const cookies = new Cookies();
+    const login = () => {
+      Axios.post("http://localhost:3001/login",{username,password}).then(res => {
+        const { firstName, lastName, username,token, userId } = res.data;
+        cookies.set("token",token); 
+        cookies.set("userId",userId); 
+        cookies.set("username",username); 
+        cookies.set("firstName",firstName); 
+        cookies.set("lastName",lastName); 
+      });
+    };
   return (
     <div className = "background">
         <div className="form-div">
@@ -22,7 +34,7 @@ function Login() {
 
             <div className ="input-container">
             <label className = "label"> Password: </label>
-            <input className = "input" placeholder = "Password" onChange = {(event)=> setPassword(event.target.value)}/>
+            <input className = "input" type="password" placeholder = "Password" onChange = {(event)=> setPassword(event.target.value)}/>
             </div>
 
             <button className = "button" onClick ={login}> Login </button>
