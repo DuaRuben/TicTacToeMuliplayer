b0c2cc9345bc6892c6342c70d710067b7675314c
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2024-09-22 17:47:50 +0530

    try

--- client/src/App.js
+++ client/src/App.js
@@ -39,9 +39,88 @@ function App() {
        
           <Route path="/signup" element={<SignUp />} />
         </Routes>
+  const navigate = useNavigate();
+  const client = StreamChat.getInstance(api_key);
+
+  const [isAuth, setAuth] =  useState(false);
+
+  useEffect(()=>{
+    if(token){
+      client.connectUser(
+        {
+        id: cookies.get("userId"),
+        name: cookies.get("username"),
+        firstName: cookies.get("firstName"),
+        lastName: cookies.get("lastName"),
+        hashedPassword: cookies.get("hashedPassword"),
+        },
+        token
+      ).then((user) =>{
+        setAuth(true);
+      });
+    }
+  },[token,client, cookies]);
+
+  return (
+    <div className="App">
+      <Router>
+        <Header isAuth={isAuth} setAuth={setAuth} client={client} cookies ={cookies} navigate ={navigate}/>
+        <AppRoutes isAuth={isAuth} setAuth={setAuth} client={client} cookies={cookies} />
       </Router>
     </div>
   );
 }
+function AppRoutes({ isAuth, setAuth, client, cookies }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate("/game");
+    }
+  }, [isAuth, navigate]);
+
+  return (
+    <>
+      <Routes>
+        {isAuth ? (
+          <>
+            <Route path="/game" element={<Game />} />
+            <Route path="*" element={<Game/>} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Login setAuth={setAuth} />} />
+            <Route path="/signup" element={<SignUp setAuth={setAuth} />} />
+            <Route path="*" element={<Login setAuth={setAuth} />} />
+          </>
+        )}
+      </Routes>
+    </>
+  );
+}
+
+//   return (
+//     <div className="App">
+//       <Router>
+//         < Header />
+//         {isAuth && <button onClick={LogOut}>Log Out</button>}
+//         <Routes>
+//           {isAuth ? (
+//             <>
+//              <Route path="/game" element={<Game />} />
+//              <Route path="*" element={<Game />} />
+//             </>
+//           ):(
+//             <>
+//             <Route path="/" element={<Login setAuth={setAuth}  />} />
+//             <Route path="/signup" element={<SignUp setAuth={setAuth} />} />
+//             <Route path="*" element={<Login setAuth={setAuth} />} />
+//             </>
+//           )}
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
 
 export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import Cookies from "universal-cookie";
+import { StreamChat } from "stream-chat";
 import "./Header.css"
 
-function Header() {
+function Header({isAuth, setAuth, client, cookies, navigate}) {
+
+  const api_key = "ysvphnrcgte8";
+
+  const LogOut = () => {
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("username");
+    cookies.remove("firstName");
+    cookies.remove("lastName");
+    cookies.remove("hashedPassword");
+    cookies.remove("channelName");
+    client.disconnectUser();
+    setAuth(false);
+    navigate("/");
+  };
+
   return (
     <div className = "header">
         <h1 className = "heading"> Tic Tac Toe </h1>
+        {isAuth && <button onClick={LogOut}>Log Out</button>}
     </div>
   )
 }
