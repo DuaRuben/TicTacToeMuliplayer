2fb0bbf21c514777bee5d90b4d281abb248dfd87
Ruben <duaruben18@gmail.com>
2024-12-14 21:49:01 -0800

    Updates to Log out button

--- client/src/App.js
+++ client/src/App.js
@@ -62,7 +62,7 @@ function App() {
   return (
     <div className="App">
         < Header/>
-        {isAuth && <button onClick={logOut}>Log Out</button>}
+        {isAuth && <button class = "logOutbutton" onClick={logOut}>Log Out</button>}
         <Routes>
           {isAuth ? (
             <>
