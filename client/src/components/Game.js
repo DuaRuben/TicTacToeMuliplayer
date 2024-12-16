17e1d28b929f57a0655c58c0bd0981338eb4be95
Ruben <duaruben18@gmail.com>
2024-12-15 17:39:56 -0800

    Update Game.js
    Adding the functionality to get the result of the game

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import Board from './Board'
 
 function Game({channel}) {
     const [playersJoined, setPlayersJoined] = useState(channel.state.watcher_count === 2);
-
+    const [result, setResult] = useState({winner:"none",state:"none"})
     channel.on("user.watching.start",(event) =>{
         setPlayersJoined(event.watcher_count === 2)
     });
@@ -10,7 +11,9 @@ function Game({channel}) {
         return <div> Waiting for the other player to connect... </div>
     }
   return (
-    <div>game</div>
+    <div className = "gameContainer">
+        <Board result ={result} setResult={setResult}/>
+    </div>
   )
 }
 
