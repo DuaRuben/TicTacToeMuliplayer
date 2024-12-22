52981dd24717c1d6431abbd4afa0459d9d017a24
Ruben <duaruben18@gmail.com>
2024-12-22 13:55:11 -0800

    Update About.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,13 +9,23 @@ function About() {
         <section className="aboutContainer">
           <h1 className="aboutTitle">About</h1>
           <p>
-            Tic Tac Toe is a timeless game loved by players of all ages. Two players alternate turns, placing X's and O's on a 3x3 grid, with the goal of aligning three marks horizontally, vertically, or diagonally. <br />
-            This web-based version brings the excitement of Tic Tac Toe to your fingertips. Compete with friends or family and showcase your strategic skills. Who will emerge victorious? <br />
-            Ready to play? Let the challenge begin!
+            Tic Tac Toe is a timeless game loved by players of all ages. Two players alternate turns, 
+            placing <span className="highlight">X's</span> and <span className="highlight">O's</span> on a 
+            3x3 grid, with the goal of aligning three marks horizontally, vertically, or diagonally.
+          </p>
+          <p>
+            This web-based version brings the excitement of Tic Tac Toe to your fingertips. 
+            Compete with friends or family and showcase your strategic skills. 
+            Who will emerge victorious?
+          </p>
+          <p className="cta">
+            Ready to play? <span className="challenge">Let the challenge begin!</span>
           </p>
         </section>
         <section className = "interactiveBoardContainer">
-          <InteractiveBoard/>
+            <div className = "" >
+            </div>
+            <InteractiveBoard/>
         </section>
       </section>
     </div>
