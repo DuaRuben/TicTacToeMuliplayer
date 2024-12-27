0139e94a9aaeef45a1021c159f10764b0807ecdf
Ruben <duaruben18@gmail.com>
2024-12-27 01:20:21 -0800

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -22,6 +22,9 @@ const api_key = process.env.API_KEY;
 const api_secret = process.env.API_SECRET;
 const serverClient = StreamChat.getInstance(api_key,api_secret);
 
+app.get("/api",async(req,res)=>{
+    res.json({message:"Hello"});
+})
 
 app.post("/api/signup",async( req,res) =>{
     try {
